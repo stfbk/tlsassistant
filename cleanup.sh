48e8a4294fcf7fc85f0ec7ae9e01ef800224a963
Salvatore <smanfredi@fbk.eu>
2020-01-27 13:42:32 +0100

    Updated dependencies

--- cleanup.sh
+++ cleanup.sh
@@ -6,5 +6,6 @@ rm -rf Analyzer/tools/others/mallodroid 2>/dev/null
 rm -rf Analyzer/tools/server/testssl.sh-3.0rc5 2>/dev/null
 rm -rf Analyzer/tools/server/TLS_Extended_Master_Checker 2>/dev/null
 rm -rf Analyzer/tools/server/tlsfuzzer 2>/dev/null
+rm -rf Report 2>/dev/null
 
 echo -e '\033[1mDeletion completed successfully!\033[0m'
