ff08f74710959b81a1f51b3b451bc6fad80915f7
matteo <32160291+Matteounitn@users.noreply.github.com>
2021-03-30 15:11:54 +0200

    comments and better formatting

--- install.py
+++ install.py
@@ -11,88 +11,111 @@ import argparse
 import logging
 from shutil import rmtree as rm_rf
 
-parser = argparse.ArgumentParser(description="Installer for TLSAssistant")
-parser.add_argument("-v", "--verbose", help="Verbose mode.", action="store_true")
+# parser for the arguments
+parser = argparse.ArgumentParser(
+    description="Installer for TLSAssistant"
+)  # todo: edit the description of the tool
+parser.add_argument(
+    "-v", "--verbose", help="Verbose mode.", action="store_true"
+)  # verbose flag
 
-args = parser.parse_args()
+args = parser.parse_args()  # parse arguments
 
-if args.verbose:
-    logging.basicConfig(level=logging.DEBUG)
+if args.verbose:  # if verbose is set
+    logging.basicConfig(level=logging.DEBUG)  # logger is set to debug
 else:
-    logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO)  # logger is set to info
 
 
 class Install:
-    def __init__(self, dependencies):
+    def __init__(self, dependencies):  # constructor
         gits = []
         pkgs = []
         zips = []
         logging.info("Loading dependencies...")
-        for dependency in dependencies:
-            if dependency["type"] == "git":
-                gits.append(dependency["url"])
+        for dependency in dependencies:  # for each dependency
+            if dependency["type"] == "git":  # if it's git
+                gits.append(dependency["url"])  # append it's url to the git array
                 logging.debug(f"Added dependency git {dependency['url']}")
-            elif dependency["type"] == "pkg":
-                pkgs.append(dependency["url"])
+            elif dependency["type"] == "pkg":  # if it's pkg
+                pkgs.append(dependency["url"])  # append it's url to the pkg array
                 logging.debug(f"Added dependency pkg {dependency['url']}")
-            elif dependency["type"] == "zip":
-                zips.append(dependency["url"])
+            elif dependency["type"] == "zip":  # if it's zip
+                zips.append(dependency["url"])  # append it's url to the zip array
                 logging.debug(f"Added dependency zip {dependency['url']}")
-            else:
+            else:  # if not found, throw warning
                 logging.warning(
                     f"Ignoring dependency {dependency['url']}, type {dependency['type']} is not recognized."
                 )
         logging.info("Getting files...")
         logging.debug("Getting all pkgs...")
-        loop = asyncio.get_event_loop()
-        results_pkgs = loop.run_until_complete(self.download(pkgs))
+        loop = asyncio.get_event_loop()  # asnychronous event loop
+        results_pkgs = loop.run_until_complete(
+            self.download(pkgs)
+        )  # download asynchronously all the files
         logging.debug(results_pkgs)
         logging.debug("Getting all zips...")
         loop = asyncio.get_event_loop()
         results_zips = loop.run_until_complete(self.download(zips))
         logging.debug(results_zips)
         logging.debug("Getting all git...")
-        for git in gits:
-            file_name = self.get_filename(git)
+        for git in gits:  # for each git url,
+            file_name = self.get_filename(git)  # get the file name
             logging.info(f"getting {file_name}...")
-            self.git_clone(git)
+            self.git_clone(git)  # and clone it
             logging.info(f"{file_name} done.")
 
         logging.info("Installing dependencies...")
-        self.install_dependencies('pkgs', results_pkgs)
+        self.install_dependencies("pkgs", results_pkgs)  # install the dependencies pkg
         logging.info("Unzipping dependencies...")
-        self.install_dependencies('zips', results_zips)
+        self.install_dependencies("zips", results_zips)  # unzips the zips
         logging.info("All done!")
 
     def install_dependencies(self, type, results):
 
         for file in results:
-            if type == 'pkgs':
+            if type == "pkgs":
                 logging.debug(f"Installing dependencies{sep}{file}")
-                with open(devnull, 'w') as null:
+                with open(devnull, "w") as null:
                     subprocess.check_call(
-                        ["sudo", "apt-get", "install", "-y", f"./dependencies{sep}{file}"],
+                        [
+                            "sudo",
+                            "apt-get",
+                            "install",
+                            "-y",
+                            f"./dependencies{sep}{file}",
+                        ],
                         stderr=sys.stderr,
                         stdout=(
                             sys.stdout
-                            if logging.getLogger().isEnabledFor(logging.DEBUG)
-                            else null
+                            if logging.getLogger().isEnabledFor(
+                                logging.DEBUG
+                            )  # if the user asked for debug mode, let him see the output.
+                            else null  # else /dev/null
                         ),
                     )
-            elif type == 'zips':
+            elif type == "zips":
                 logging.debug(f"Unzipping dependencies{sep}{file}")
-                with ZipFile(f"dependencies{sep}{file}", 'r') as zip:
-                    zip.extractall(f"dependencies{sep}{file.rsplit('.', 1)[0]}")
-            else:
-                logging.error('no type found.')
-                raise AssertionError("The type given doesn't match one of the existing one.")
-            if path.exists(f"dependencies{sep}{file}"):
+                with ZipFile(
+                    f"dependencies{sep}{file}", "r"
+                ) as zip:  # while opening the zip
+                    zip.extractall(
+                        f"dependencies{sep}{file.rsplit('.', 1)[0]}"
+                    )  # extract it and remove the extension (myzip.zip) in the folder myzip
+            else:  # if the type is not found, stop everything, we have an issue.
+                logging.error("no type found.")
+                raise AssertionError(
+                    "The type given doesn't match one of the existing one."
+                )
+            if path.exists(
+                f"dependencies{sep}{file}"
+            ):  # delete the files .deb and .zip after all.
                 logging.debug(f"Removing file dependencies{sep}{file}")
                 remove(f"dependencies{sep}{file}")
 
     def git_clone(self, url, path=None):
         file_name = self.get_filename(url)
-        with open(devnull, 'w') as null:
+        with open(devnull, "w") as null:
             subprocess.call(
                 [
                     "git",
@@ -100,9 +123,13 @@ class Install:
                     str(url),
                     f"{path if path else 'dependencies' + sep + file_name}",
                 ],
-                stderr=sys.stderr if logging.getLogger().isEnabledFor(logging.DEBUG) else null,
+                stderr=sys.stderr
+                if logging.getLogger().isEnabledFor(logging.DEBUG)
+                else null,
                 stdout=(
-                    sys.stdout if logging.getLogger().isEnabledFor(logging.DEBUG) else null
+                    sys.stdout
+                    if logging.getLogger().isEnabledFor(logging.DEBUG)
+                    else null
                 ),
             )
 
@@ -118,13 +145,13 @@ class Install:
                         # logging.debug(f"Downloaded {url} in {file_name}")
         return file_name
 
-    async def download(self, urls):
+    async def download(self, urls):  # download asynchonously for faster downloads
         async with aiohttp.ClientSession() as session:
-            tasks = [self.get_url(url, session) for url in urls]
+            tasks = [self.get_url(url, session) for url in urls]  # load tasks
 
-            return await asyncio.gather(*tasks)
+            return await asyncio.gather(*tasks)  # and gather them
 
-    def get_filename(self, url):
+    def get_filename(self, url):  # used to split and get the file name from http url
         fragment_removed = url.split("#")[0]
         query_string_removed = fragment_removed.split("?")[0]
         scheme_removed = query_string_removed.split("://")[-1].split(":")[-1]
@@ -135,25 +162,27 @@ class Install:
 
 
 def main():  # exec main
-    if not path.exists("dependencies"):
+    if not path.exists("dependencies"):  # if can't find dependency folder
         logging.debug("Folder dependencies does not exist. Creating a new one.")
     else:
         logging.debug("Folder dependencies exist. Removing and creating a new one.")
-        rm_rf("dependencies")
-    mkdir("dependencies")
-    if path.exists("dependencies.json"):
+        rm_rf("dependencies")  # delete the folder
+    mkdir("dependencies")  # create the folder
+    if path.exists("dependencies.json"):  # if  find the dependency file
         with open("dependencies.json", "r") as dep:  # load dependencies
             data = dep.read()
             dependencies = json.loads(data)
             Install(dependencies)  # install dependencies
-    else:
+    else:  # there's no file dependencies.json
         logging.error("File not found, dependency links are missing. Abort.")
         raise FileNotFoundError("File dependencies is not found, Abort.")
 
 
 if __name__ == "__main__":
     if geteuid() == 0:  # check if sudo
-        main()
+        main()  # start
     else:
-        logging.warning("This tools need SUDO to work.")
-        subprocess.check_call(["sudo", sys.executable] + sys.argv)  # force sudo
+        logging.warning("This tools need SUDO to work!")
+        subprocess.check_call(
+            ["sudo", sys.executable] + sys.argv
+        )  # force sudo, rerun the script as sudo
