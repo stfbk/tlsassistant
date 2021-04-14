083dcc0628ab773ace3dc5b582b4ac74c997b7f6
matteo <32160291+matteotn@users.noreply.github.com>
2021-04-14 11:58:23 +0200

    added new requirements to parse urls

--- utils/urls.py
+++ utils/urls.py
@@ -1,6 +1,14 @@
 import re
+from tldextract import extract
 
 
-def url_strip(input_url,strip_www=False):
-    url = re.compile(r"https?://") if not strip_www else re.compile(r"https?://(www\.)?")
-    return url.sub('', input_url).strip().strip('/')
+def url_strip(input_url, strip_www=False):
+    url = (
+        re.compile(r"https?://") if not strip_www else re.compile(r"https?://(www\.)?")
+    )
+    return url.sub("", input_url).strip().strip("/")
+
+
+def url_tld(url) -> str:
+    results = extract(url)
+    return f"{results.domain}.{results.suffix}"
