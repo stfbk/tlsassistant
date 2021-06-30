d9a01e02f2934dab2d057ea44d0d9067d44c7a0a
matteo <32160291+matteotn@users.noreply.github.com>
2021-06-30 17:50:21 +0200

    Added sweet32 as a working example, added openssl checks, added auto

--- modules/configuration/configuration.py
+++ modules/configuration/configuration.py
@@ -21,6 +21,16 @@ class Configuration:
         self.__logging = Logger("Configuration APACHE/NGINX")
         self.__loaded_conf = self.__load_conf(path)
 
+    def __obtain_vhost(self):
+        assert self.__type != self.Type.AUTO, "Can't use this method with AUTO type."
+        if self.__type == self.Type.APACHE:
+            if 'VirtualHost' not in self.__loaded_conf:
+                self.__loaded_conf['VirtualHost'] = []
+            for vhost in self.__loaded_conf['VirtualHost']:
+                yield vhost
+        elif self.__type == self.Type.NGINX:
+            raise NotImplementedError
+
     def __load_conf(self, path) -> dict:
         file = Path(path)
         assert (
@@ -29,11 +39,13 @@ class Configuration:
         if self.__type == self.Type.AUTO:
             try:
                 results = self.__load_apache_conf(file)
+                self.__type = self.Type.APACHE
             except Exception as e:
                 self.__logging.debug(
                     f"Couldn't parse config as apache: {e}\ntrying with nginx..."
                 )
                 results = self.__load_nginx_conf(file)
+                self.__type = self.Type.NGINX
         elif self.__type == self.Type.APACHE:
             results = self.__load_apache_conf(file)
         else:
@@ -50,19 +62,31 @@ class Configuration:
     def __is_config_enabled(self, module) -> bool:
         return hasattr(module, "conf") and isinstance(module.conf, Config_base)
 
-    def is_vuln(self, modules: dict):
+    def __wrapper(self, modules: dict, fix=False, openssl: str = None, ignore_openssl: bool = False):
         boolean_results = {}
         for name, module in modules.items():
-            for (
-                vhost_name,
-                vhost,
-            ) in self.__loaded_conf.items():  # todo: check if true, it's pseudocode
-                if self.__is_config_enabled(module):
-                    if vhost_name not in boolean_results:
-                        boolean_results[vhost_name] = {}
-                    boolean_results[vhost_name][name] = module.conf.condition(vhost)
-                else:
-                    self.__logging.warning(
-                        f"The module {name} isn't compatible. Skipping..."
-                    )
+            for virtualhost in self.__obtain_vhost():
+                for vhost_name, vhost in virtualhost.items():
+                    if self.__is_config_enabled(module):
+                        self.__logging.debug(f"Analyzing vulnerability {name} in vhost {vhost_name}..")
+                        if vhost_name not in boolean_results:
+                            boolean_results[vhost_name] = {}
+                        boolean_results[vhost_name][name] = module.conf.condition(vhost, openssl=openssl,
+                                                                                  ignore_openssl=ignore_openssl)
+                        if fix:
+                            if boolean_results[vhost_name][name]:
+                                self.__logging.debug(f"Fixing vulnerability {name} in vhost {vhost_name}..")
+                                module.conf.fix(vhost)
+                    else:
+                        self.__logging.warning(
+                            f"The module {name} isn't compatible. Skipping..."
+                        )
         return boolean_results
+
+    def is_vuln(self, modules: dict, openssl=None, ignore_openssl=False):
+        self.__logging.info("Checking for vulnerabilities...")
+        return self.__wrapper(modules, openssl=openssl, ignore_openssl=ignore_openssl)
+
+    def fix(self, modules: dict, openssl=None, ignore_openssl=False):
+        self.__logging.info("Fixing vulnerabilities...")
+        return self.__wrapper(modules, fix=True, openssl=openssl, ignore_openssl=ignore_openssl)
--- modules/configuration/configuration_base.py
+++ modules/configuration/configuration_base.py
@@ -1,3 +1,34 @@
+from ssl import OPENSSL_VERSION
+
+
+class OpenSSL:
+    VERSION = OPENSSL_VERSION.split()[1]
+
+    def less_than(self, ver1, ver2=VERSION):
+        return self.__compare(ver1, ver2)
+
+    def greater_than(self, ver1, ver2=VERSION):
+        return self.__compare(ver1, ver2, reverse=True)
+
+    def is_safe(self, ver1, ver2=VERSION):
+        return self.less_than(ver1, ver2)
+
+    def __compare(self, ver1, ver2, reverse=False):
+        assert len(ver1) == 6 or len(ver1) == 5, "OpenSSL version must be 5 or 6 char long.\nFor example '1.1.1f'"
+        assert len(ver2) == 6 or len(ver2) == 5, "OpenSSL version must be 5 or 6 char long.\nFor example '1.1.1f'"
+        # even the versions
+        if len(ver1) == 6 and len(ver2) == 5:
+            ver1 = ver1[:-1]
+        elif len(ver2) == 6 and len(ver1) == 5:
+            ver2 = ver2[:-1]
+        return (ver1 < ver2) if not reverse else (ver1 > ver2)
+
+
 class Config_base:
-    def condition(self):
+    openSSL = OpenSSL()
+
+    def condition(self, vhost):
+        raise NotImplementedError
+
+    def fix(self, vhost):
         raise NotImplementedError
