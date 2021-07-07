3f9b4dd853455391d10d2bf4674f94091e59235f
matteo <32160291+matteotn@users.noreply.github.com>
2021-07-07 20:41:26 +0200

    Fix booleanize

--- utils/booleanize.py
+++ utils/booleanize.py
@@ -1,5 +1,9 @@
-def boolean_results(modules: list, raw_results: dict) -> dict:
+from utils.prune import pruner
+
+
+def boolean_results(modules: list or dict, raw_results: dict) -> dict:
     b_res = {}
+    res = pruner(raw_results)
     for module in modules:
-        b_res[module] = False if module in raw_results and raw_results else True
+        b_res[module] = True if module in res and res else False
     return b_res
