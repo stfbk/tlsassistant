e583e7fbb06ab03b0b4fd40c7d9e0ae6aa0342f4
matteo <32160291+matteotn@users.noreply.github.com>
2021-11-19 14:01:37 +0100

    Fix PFS detection

--- modules/server/pfs.py
+++ modules/server/pfs.py
@@ -1,4 +1,5 @@
 from modules.server.testssl_base import Testssl_base
+from utils.mitigations import load_mitigation
 
 
 class Pfs(Testssl_base):
@@ -10,6 +11,23 @@ class Pfs(Testssl_base):
     def _set_arguments(self):
         self._arguments = ["-f"]
 
+    def _set_mitigations(self, result: dict, key: str, condition: bool) -> dict:
+        """
+        Sets the mitigations for the given result
+
+        :param result: the result to set the mitigations for
+        :type result: dict
+        :param key: the key of the result
+        :type key: str
+        :param condition: the condition to set the mitigations for
+        :type condition: bool
+        :return: the mitigations for the given result
+        :rtype: dict
+        """
+        if condition:
+            result["mitigation"] = load_mitigation("PFS")
+        return result if condition else {}
+
     # to override
     def _worker(self, results):
         """
@@ -20,5 +38,5 @@ class Pfs(Testssl_base):
         :rtype: dict
         """
         return self._obtain_results(
-            results, ["PFS_ciphers", "PFS_ECDHE_curves", "DH_groups"]
+            results, ["PFS_ciphers", "PFS_ECDHE_curves", "DH_groups", "pre_128cipher", "PFS"]
         )
