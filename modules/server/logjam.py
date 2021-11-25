db05da30eadccbb176de1633674e2d8f761cb355
matteo <32160291+matteotn@users.noreply.github.com>
2021-11-25 09:02:36 +0100

    Fix missing beast mitigation, added logjam linking to the mitigation

--- modules/server/logjam.py
+++ modules/server/logjam.py
@@ -1,8 +1,8 @@
 from modules.server.testssl_base import Testssl_base
+from utils.mitigations import load_mitigation
 
 
 class Logjam(Testssl_base):
-
     """
     Analysis of the logjam testssl results
     """
@@ -13,6 +13,23 @@ class Logjam(Testssl_base):
         Sets the arguments for the testssl command
         """
         self._arguments = ["-J"]
+
+    def _set_mitigations(self, result: dict, key: str, condition: bool) -> dict:
+        """
+        Override the _set_mitigations method to add the logjam results
+
+        :param result: the results of the testssl command
+        :type result: dict
+        :param key: the key of the result
+        :type key: str
+        :param condition: the condition to check
+        :type condition: bool
+        :return: the result with the mitzvah results
+        :rtype: dict
+        """
+        if condition:
+            result["mitigation"] = load_mitigation("LOGJAM")
+        return result if condition else {}
 
     # to override
     def _worker(self, results):
