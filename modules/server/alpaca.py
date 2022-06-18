6fe882c0c9d6498f2aacbb4a19ee21ffc294d72d
IvanValentini <34308333+IvanValentini@users.noreply.github.com>
2022-06-18 13:39:40 +0200

    ALPACA Apache Dynamic Mitigations

--- modules/server/alpaca.py
+++ modules/server/alpaca.py
@@ -44,24 +44,48 @@ class Alpaca(TLS_Scanner_base):
             if "ALPN" or "SNI" in ext:
                 result['mitigation']['Entry']['Mitigation']['Nginx'] = ""
             if "ALPN" in ext:
-                result['mitigation']['Entry']['Mitigation']['Nginx'] = "To enable Strict ALPN in Nginx upgrade to version >=1.21.4. <br>"
+                result['mitigation']['Entry']['Mitigation']['Nginx'] = "To enable Strict ALPN in Nginx upgrade to version >=1.21.4. <br/><br/>"
+                result['mitigation']['Entry']['Mitigation']['Apache'] = "Currenlty there is no snippet available to enable Strict ALPN. <br/><br/>"
             if "SNI" in ext:               
                 result['mitigation']['Entry']['Mitigation']['Nginx'] += """
-                To enable Strict SNI in Nginx: <br> 
-                1. If you are using Nginx>=1.19.4 edit you configuration file usually located in <i>/etc/nginx/sites-enabled/default</i><br/> (if you changed your site conf name <i>/etc/nginx/sites-enabled/YOURSITECONFIGURATION</i>); to look like this:<br>
-                <code>server {<br>
-                    listen               443 ssl default_server;<br>
-                    ssl_reject_handshake on;<br>
-                }<br>
-                <br>
-                server {<br>
-                    listen 443 ssl http2;<br>
-                    listen [::]:443 ssl http2;<br>
-                    server_name example.com;<br>
-                    [...]<br>
-                }<br>
-                </code><br>
-                2. If you are using nginx<1.19.4 follow <a href="https://blog.sion.moe/methods-to-prevent-leaking-websites-origin-server-ip-behind-cdn/">this guide</a><br>
+                To enable Strict SNI in Nginx: <br/> 
+                1. If you are using Nginx>=1.19.4 edit you configuration file usually located in <i>/etc/nginx/sites-enabled/default</i><br/> (if you changed your site conf name <i>/etc/nginx/sites-enabled/YOURSITECONFIGURATION</i>); to look like this:<br/>
+                <code>server {<br/>
+                    listen               443 ssl default_server;<br/>
+                    ssl_reject_handshake on;<br/>
+                }<br/>
+                <br/>
+                server {<br/>
+                    listen 443 ssl http2;<br/>
+                    listen [::]:443 ssl http2;<br/>
+                    server_name example.com;<br/>
+                    [...]<br/>
+                }<br/>
+                </code><br/>
+                2. If you are using nginx<1.19.4 follow <a href="https://blog.sion.moe/methods-to-prevent-leaking-websites-origin-server-ip-behind-cdn/">this guide</a><br/>
+                """
+                result['mitigation']['Entry']['Mitigation']['Apache'] += """
+                To enable Strict SNI in Apache: <br/> 
+                1. The first step is to create a dummy certificate for localhost with a 100-year lifespan (unless you feel like setting up a cron job that regenerates a 90-day certificate every 30 days or so):<br/>
+                <code>
+                sudo openssl genrsa -out /etc/ssl/private/localhost.key 2048<br/>
+                sudo openssl req -new -key /etc/ssl/private/localhost.key -subj /CN=localhost -out /etc/ssl/certs/localhost.csr<br/>
+                echo subjectAltName=DNS:localhost | sudo openssl x509  -in /etc/ssl/certs/localhost.csr -out /etc/ssl/certs/localhost.crt -req -signkey /etc/ssl/private/localhost.key -days 36525 -extfile - <br/>
+                </code><br/>
+                2. Create the virtual host configuration which uses that certificate so that TLS connections without a server name will now be directed to your dummy virtual host and fail:<br/> 
+                <code>
+                &lt;VirtualHost *:443&gt;<br/>
+                ServerName localhost<br/>
+                SSLEngine On<br/>
+                SSLStrictSNIVHostCheck On<br/>
+                SSLCertificateKeyFile /etc/ssl/private/localhost.key<br/>
+                SSLCertificateFile /etc/ssl/certs/localhost.crt<br/>
+                &lt;Location /&gt;<br/>
+                    Require all denied<br/>
+                &lt;/Location&gt;<br/>
+                &lt;/VirtualHost&gt;<br/>
+                </code><br/>
+                This must be placed in a location where it will be read before any other virtual host configuration, for example before all other virtual host configurations in: <i>/etc/apache2/sites-available/example.com.conf</i>. 
                 """
         return result if condition else {}
 
