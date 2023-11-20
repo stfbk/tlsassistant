5e620096c2848e7afe8b7134b89579b66091fed1
Odinmylord <rgermenia@gmail.com>
2023-11-20 15:17:43 +0100

    Updated testssl and added error if DNS resolution fails

--- modules/server/wrappers/testssl.py
+++ modules/server/wrappers/testssl.py
@@ -68,7 +68,7 @@ class Testssl:
         """
         Loads testssl variables.
         """
-        self.__testssl = f"dependencies{sep}3.0.4{sep}testssl.sh-3.0.4{sep}testssl.sh"
+        self.__testssl = f"dependencies{sep}v3.2rc3{sep}testssl.sh-3.2rc3{sep}testssl.sh"
         self.__input_dict = {}
 
     def input(self, **kwargs):
