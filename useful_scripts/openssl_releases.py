5dc1b7a52f0c8ef5be3d84f14a96d76c64230f68
Odinmylord <rgermenia@gmail.com>
2024-01-30 16:12:18 +0100

    added more openssl versions and default groups

--- useful_scripts/openssl_releases.py
+++ useful_scripts/openssl_releases.py
@@ -1,10 +1,14 @@
+import pprint
+
 import requests
 import os
 import tarfile
 import shutil
 import json
 import time
-releases = ["1.0.0", "1.0.1", "1.0.2", "1.1.0", "1.1.1", "3.0", "3.1", "3.2"]
+import logging
+logging.basicConfig(level=logging.DEBUG)
+releases = ["0.9.x", "1.0.0", "1.0.1", "1.0.2", "1.1.0", "1.1.1", "3.0", "3.1", "3.2"]
 # THIS SCRIPT IS USED TO DOWNLOAD THE OPENSSL RELEASES AND EXTRACT THE SIGALGS
 # FROM THE SOURCE CODE. THE SIGALGS ARE THEN USED IN THE CONFIGURATION FILES
 # FOR THE COMPLIANCE MODULE.
@@ -25,7 +29,7 @@ def download_releases():
             lines = r.text.split("\n")
             for line in lines:
                 if "openssl-" in line and ".tar.gz" in line:
-                     old_releases[release].append(line.split(">")[1].split("<")[0])
+                    old_releases[release].append(line.split(">")[1].split("<")[0])
             time.sleep(0.2)
         urls = {}
         for release in latest_releases:
@@ -34,7 +38,7 @@ def download_releases():
             for old_release in old_releases[release]:
                 urls[old_release.strip(".tar.gz")] = f"https://www.openssl.org/source/old/{release}/{old_release}"
         with open("urls.json", "w") as f:
-            json.dump(urls, f, indent=4)
+            json.dump(urls, f, indent=4, sort_keys=True)
     else:
         with open("urls.json", "r") as f:
             urls = json.load(f)
@@ -59,33 +63,86 @@ def download_releases():
 def extract_file(release, file):
     tar = tarfile.open(f"tmp/{release}.tar.gz")
     first_name = tar.getnames()[0]
-    if f"{first_name}/ssl/{file}" in tar.getnames():
-        first_name = first_name.split("/")[0] if "/" in first_name else first_name
-        tar.extract(f"{first_name}/ssl/{file}", path=f"tmp/")
+    if "/" in first_name:
+        first_name = first_name.split("/")[0]
+    member_string = f"{first_name}/ssl/{file}"
+    if member_string in tar.getnames():
+        obj = tar.getmember(member_string)
+        tar.extract(obj, path=f"tmp/")
         tar.close()
+        if not os.path.isdir(f"tmp/{release}"):
+            os.mkdir(f"tmp/{release}")
         shutil.move(f"tmp/{first_name}/ssl/{file}", f"tmp/{release}/{file}")
         if first_name != release:
             if not os.path.exists(f"tmp/{release}"):
                 os.mkdir(f"tmp/{release}")
             shutil.rmtree(f"tmp/{first_name}")
 
-def extract_sigalgs():
-    sigalgs_dict = {}
-    sigalgs_table = {}
+def extract_tables():
+    pairs = {
+        "sigalg_lookup_tbl[]": {
+            "end": "SIGALG_LOOKUP",
+            "key": "sigalgs"
+        },
+        "supported_groups_default[]": {
+            "end": "};",
+            "key": "groups_default"
+        },
+        "eccurves_default[]": {
+            "end": "};",
+            "key": "groups_default"
+        },
+    }
+    tables = {}
     for release in [r for r in os.listdir("tmp") if r[-2:] != "gz" and r[-3:] != "csv"]:
-        lines = []
         print("Release: ", release)
         with open(f"tmp/{release}/t1_lib.c", "r") as f:
+            tables[release] = {}
             line = "a"
             start_reading = False
+            end = ""
             while line:
                 line = f.readline()
-                if "static const SIGALG_LOOKUP sigalg_lookup_tbl[]" in line:
-                    start_reading = True
-                elif "SIGALG_LOOKUP" in line:
+                if not start_reading:
+                    for key in pairs:
+                        if key in line:
+                            start_reading = pairs[key]["key"]
+                            tables[release][start_reading] = []
+                            end = pairs[key]["end"]
+                elif end and end in line:
                     start_reading = False
+                    end = ""
                 elif start_reading:
-                    lines.append(line)
+                    tables[release][start_reading].append(line)
+    extract_sigalgs(tables)
+    extract_groups(tables)
+
+def extract_groups(releases_data):
+    groups_dict = {}
+    for release in releases_data:
+        lines = releases_data[release].get("groups_default", [])
+        groups = []
+        for l in lines:
+            if "/*" in l:
+                # remove also */ from the line
+                l = l.split("/*")[1].strip()[:-2]
+                if " " in l:
+                    l = l.split(" ")[0]
+                # Don't add gost groups because they are not enabled by default in openssl
+                if not l.startswith("GC") and len(l) > 6:
+                    groups.append(l)
+        if not lines:
+            groups = ["prime256v1"]
+        release = release.lower().replace("openssl", "")[1:]
+        groups_dict[release] = groups
+    with open("../configs/compliance/groups.json", "w") as f:
+        json.dump(groups_dict, f, indent=4, sort_keys=True)
+
+def extract_sigalgs(releases_data):
+    sigalgs_dict = {}
+    sigalgs_table = {}
+    for release in releases_data:
+        lines = releases_data[release].get("sigalgs", [])
         sigalgs = []
         for l in lines:
             if "TLSEXT_SIGALG" in l and "gost" not in l:
@@ -141,4 +198,4 @@ if __name__ == "__main__":
     if not os.path.exists("tmp"):
         os.mkdir("tmp")
     download_releases()
-    extract_sigalgs()
+    extract_tables()
