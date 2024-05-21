f724a21060bb2c0d0efe3cd3147a79a76a4ea0ed
MrAleFonta <alessandro.fontana.639@gmail.com>
2024-05-21 18:43:21 +0200

    create wrapper of SEBASTiAn

--- modules/android/wrappers/sebastian.py
+++ modules/android/wrappers/sebastian.py
@@ -11,7 +11,16 @@ from utils.loader import load_module
 
 import sys
 sys.path.append("dependencies/SEBASTiAn/src")
-from SEBASTiAn.main import perform_analysis_with_timeout
+#from SEBASTiAn.main import perform_analysis_with_timeout
+import logging
+import os
+from datetime import datetime
+from typing import List
+from pebble import ProcessPool
+from SEBASTiAn import util
+from SEBASTiAn.analysis import AndroidAnalysis, IOSAnalysis
+from SEBASTiAn.manager import AndroidVulnerabilityManager, IOSVulnerabilityManager
+from SEBASTiAn.vulnerability import VulnerabilityDetails
 
 class Sebastian:
     """
@@ -106,9 +115,99 @@ class Sebastian:
                 f"Analysis of {file_id} (cache miss or forced by call)"
             )
             try:
-                self.__cache[file_id] = perform_analysis_with_timeout(file_id,timeout=60000)
+                self.__cache[file_id] = self.perform_analysis_with_timeout(file_id,plugins=["AllowAllHostname","CryptoEcbCipher","DebuggableApplication","DefaultSchemeHttp","InsecureConnection","InsecureHostnameVerifier","InsecureSocket","InsecureSocketFactory","InvalidServerCertificate","SEBASTiAn"],timeout=600)
             except Exception as e:
                 self.__logging.error(f"Analysis of {file_id} crashed: {e}")
         else:
             if file_id not in self.__cache:  # if not in cache, force analysis
                 self.__worker(path, args, force=True)
+
+
+    ############################################
+    #         Wrappers of SEBASTiAn            #
+    ############################################
+    def perform_analysis_without_timeout(
+        self,
+        input_app_path: str,
+        language: str = "en",
+        ignore_libs: bool = False,
+        fail_fast: bool = False,
+        keep_files: bool = False,
+        generate_report: bool = False,
+        plugins: List[str] = None,  
+    ) -> dict:
+        """
+        This method wrap the method SEBASTiAn, allowing you to choose which plugin to analyze
+        """
+        analysis = None
+        found_vulnerabilities: List[VulnerabilityDetails] = []
+        failures = 0
+        platform="Android"
+        try:
+            manager = AndroidVulnerabilityManager()
+            analysis = AndroidAnalysis(
+                input_app_path, language, ignore_libs, generate_report
+            )
+            analysis.initialize()
+
+            # Get the list of all available plugins
+            all_plugins = [item.plugin_object.__class__.__name__ for item in manager.get_all_vulnerability_checks()]
+            if plugins:
+                # Verify if the specified plugins exist
+                non_existing_plugins = [plugin for plugin in plugins if plugin not in all_plugins]
+                if non_existing_plugins:
+                    self.__logging.error(f"The following specified plugins do not exist: {', '.join(non_existing_plugins)}")
+                # Filter the vulnerability checks to include only the specified plugins
+                filtered_checks = [item for item in manager.get_all_vulnerability_checks() if item.plugin_object.__class__.__name__ in plugins]
+            else:
+                filtered_checks = manager.get_all_vulnerability_checks()
+            for item in filtered_checks:
+                try:
+                    vulnerability_details = item.plugin_object.check_vulnerability(analysis)
+                    if vulnerability_details:
+                        found_vulnerabilities.append(vulnerability_details)
+                except Exception:
+                    failures += 1
+                    if fail_fast:
+                        # Make the entire vulnerability analysis fail only if the
+                        # corresponding flag is enabled.
+                        raise
+                    
+        except Exception as e:
+            self.__logging.error(f"Vulnerability analysis failed: {e}")
+            raise
+
+        else:
+            result = {
+                "vulnerabilities": VulnerabilityDetails.Schema().dump(found_vulnerabilities, many=True)
+            }
+            return result
+        
+    def perform_analysis_with_timeout(
+        self,
+        input_app_path: str,
+        language: str = "en",
+        ignore_libs: bool = False,
+        fail_fast: bool = False,
+        keep_files: bool = False,
+        timeout: int = None,
+        generate_report: bool = False,
+        plugins: List[str] = None,  # Aggiungi il parametro plugins
+    ) -> dict:
+        '''
+        This method wrap the method SEBASTiAn, allowing you to choose which plugin to analyze
+        '''
+        with ProcessPool(1) as pool:
+            return pool.schedule(
+                self.perform_analysis_without_timeout,
+                args=[
+                    input_app_path,
+                    language,
+                    ignore_libs,
+                    fail_fast,
+                    keep_files,
+                    generate_report,
+                    plugins,  # Passa il parametro plugins qui
+                ],
+                timeout=timeout,
+            ).result()
