16bca167808a9c03ad913f72011d4569250e61a3
Riccardo Germenia <rgermenia@fbk.eu>
2026-07-07 13:30:32 +0200

    fix info messages appearing when not needed

--- tlsa/__init__.py
+++ tlsa/__init__.py
@@ -1,6 +1,8 @@
 import sys
 import os
+import logging
 
+logging.basicConfig(level=logging.WARNING)
 sys.path.append(os.path.abspath("dependencies"))
 sys.path.append(os.path.abspath("dependencies/SEBASTiAn/src"))
 sys.path.append(os.path.abspath("dependencies/tls-compliance-dataset/utils"))
